c456d0037f0b2d6c147d7b69d75b6d81b4dc2bfc
eldeivich <47783040+eldeivich@users.noreply.github.com>
2020-01-18 17:10:02 +0100

    esbozo de empleados

--- tasks/routes.py
+++ tasks/routes.py
@@ -1,6 +1,6 @@
 from tasks import app
 from flask import render_template, request, redirect, url_for
-from tasks.forms import TaskForm, ProccesTaskForm
+from tasks.forms import TaskForm, ProccesTaskForm, EmployeeForm, ProccessEmployeeForm
 
 import sqlite3
 from datetime import date
@@ -25,7 +25,7 @@ def dbQuery(consulta, *args):
         rows = rows[0]
     elif len(rows) == 0:
         rows = None
-    
+
     conn.commit()
     conn.close()
 
@@ -40,8 +40,22 @@ def index():
             registros = [registros]
     else:
         registros = []
-        
+
     return render_template("index.html", registros=registros) 
+
+@app.route("/employees")
+def employees():
+    registros = dbQuery('SELECT name, apellidos, email, id FROM empleados;')
+
+    if registros:
+        if isinstance(registros, dict):
+            registros = [registros]
+    else:
+        registros = []
+
+    return render_template("employees.html", registros=registros)
+
+
 
 @app.route("/newtask", methods=['GET', 'POST'])
 def newTask():
@@ -49,21 +63,45 @@ def newTask():
 
     if request.method == 'GET':
         return render_template("task.html", form=form)
-    else:
-        if form.validate():
-            title = request.values.get('title')
-            desc = request.values.get('description')
-            fx = request.values.get('fx')
-            
-            consulta = '''
-            INSERT INTO tareas (titulo, descripcion, fecha)
-                        VALUES (?, ?, ?);
-            '''
-            dbQuery(consulta, title, desc, fx)
 
-            return redirect(url_for("index"))
-        else:
-            return render_template("task.html", form=form)
+    if form.validate():
+        title = request.values.get('title')
+        desc = request.values.get('description')
+        fx = request.values.get('fx')
+
+        consulta = """
+        INSERT INTO tareas (titulo, descripcion, fecha)
+                    VALUES (?, ?, ?);
+        """
+        dbQuery(consulta, title, desc, fx)
+
+        return redirect(url_for("index"))
+    else:
+        return render_template("task.html", form=form)
+
+@app.route("/newemployee", methods=['GET', 'POST'])
+def newEmployee():
+    form = EmployeeForm(request.form)
+
+    if request.method == 'GET':
+        return render_template("employee.html", form=form)
+
+    if form.validate():
+        name = request.values.get('name')
+        lastname = request.values.get('lastname')
+        email = request.values.get('email')
+
+        consulta = """
+        INSERT INTO empleados (name, apellidos, email)
+                    VALUES (?, ?, ?);
+        """
+        dbQuery(consulta, name, lastname, email)
+
+        return redirect(url_for("employees"))
+    else:
+        return render_template("employee.html", form=form)
+
+
 
 @app.route("/processtask", methods=['GET', 'POST'])
 def proccesTask():
@@ -72,7 +110,7 @@ def proccesTask():
     if request.method == 'GET':
         ix = request.values.get('ix')
         if ix:
-            registroAct = dbQuery('Select titulo, descripcion, fecha, id from tareas where id = ?', ix)
+            registroAct = dbQuery('Select titulo, descripcion, fecha, id from tareas where id = ?;', ix)
 
             if registroAct:
                 if registroAct['fecha']:
@@ -89,7 +127,11 @@ def proccesTask():
                     accion = 'B'
 
 
-                form = ProccesTaskForm(data={'ix': ix, 'title': registroAct['titulo'], 'description': registroAct['descripcion'], 'fx': fechaTarea, 'btn': accion})
+                form = ProccesTaskForm(data={'ix': ix, 
+                                             'title': registroAct['titulo'], 
+                                             'description': registroAct['descripcion'], 
+                                             'fx': fechaTarea, 
+                                             'btn': accion})
 
             return render_template("processtask.html", form=form)
         else:
@@ -97,10 +139,10 @@ def proccesTask():
 
     if form.btn.data == 'B':
         ix = int(request.values.get('ix'))
-        consulta = '''
+        consulta = """
             DELETE FROM tareas
                 WHERE id = ?;
-        '''
+        """
         dbQuery(consulta, ix)
 
         return redirect(url_for('index'))
@@ -108,16 +150,72 @@ def proccesTask():
     if form.btn.data == 'M':
         if form.validate():
             ix = int(request.values.get('ix'))
-            consulta = '''
+            consulta = """
                 UPDATE tareas
                 SET titulo = ?, descripcion = ?, fecha = ?
                 WHERE id = ?;
-            '''
+            """
             dbQuery(consulta, 
-                request.values.get('title'),
-                request.values.get('description'),
-                request.values.get('fx'),
-                ix)
-            
+                    request.values.get('title'), 
+                    request.values.get('description'), 
+                    request.values.get('fx'), 
+                    ix)
+
             return redirect(url_for("index"))
         return render_template("processtask.html", form=form)
+
+@app.route("/processemployee", methods=["GET", "POST"])
+def proccesEmployee():
+    form = ProccessEmployeeForm(request.form)
+
+    if request.method == 'GET':
+        id = request.values.get('id')
+        if id:
+            registroAct = dbQuery('SELECT name, apellidos, email, id from empleados where id = ?;', id)
+
+            if registroAct:
+                accion = ''
+
+                if 'btnModificar' in request.values:
+                    accion = 'M'
+
+                if 'btnBorrar' in request.values:
+                    accion = 'B'
+
+                form = ProccessEmployeeForm(data={'id': id, 
+                                                  'name': registroAct['name'], 
+                                                  'lastname': registroAct['apellidos'], 
+                                                  'email': registroAct['email'], 
+                                                  'btn': accion})
+
+            return render_template("processemployee.html", form=form)                                
+        else:
+            return redirect(url_for("employees"))
+    
+    if form.btn.data == 'B':
+        id = int(request.values.get('id'))
+        consulta = """
+            DELETE FROM empleados
+             WHERE id = ?;
+        """
+        dbQuery(consulta, ix)
+
+        return redirect(url_for("employees"))
+
+    if form.btn.data == 'M':
+        if form.validate():
+            id = int(request.values.get('id'))
+            consulta = """
+                UPDATE empleados
+                   SET name = ?, apellidos = ?, email = ?
+                 WHERE id = ?;
+            """
+
+            dbQuery(consulta,
+                    request.values.get('name'), 
+                    request.values.get('lastname'),
+                    request.values.get('email'),
+                    id)
+
+            return redirect(url_for("employees"))
+        return render_template("processemployee.html", form=form)
